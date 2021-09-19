c1ed033b88cc62699e849b21015a28131295cb84
Stéphane de Wit <contact@stephanedewit.be>
2021-09-19 15:20:57 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var http = require('http');
+const http = require("http");
 
-var port = process.env.PORT;
-if (!port)
-	throw new Error("Can't read port configuration");
+const port = process.env.PORT;
+if (!port) {
+    throw new Error("Can't read port configuration");
+}
 
-http.createServer(function (req, res) {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+http.createServer((req, res) => {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(ip + '\n');
-}).listen(port, '0.0.0.0');
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(`${ip}\n`);
+}).listen(port, "0.0.0.0");
 
-console.log('Server running at http://0.0.0.0:' + port);
+console.log(`Server running at http://0.0.0.0:${port}`);
